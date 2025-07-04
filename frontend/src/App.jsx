7b3ed276db2e66a12a3bc863f26279f7b30ec0ee
beli <belinda.zilevu@gmail.com>
2025-07-04 12:44:53 +0200

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { CButton } from "@coreui/react";
+
+function App() {
+  return (
+    <>
+      <p className="text-primary">Hello World</p>
+      <CButton color="primary">caca</CButton>
+    </>
+  );
+}
+
+export default App;
